dd5244b7ec4d9116e2fe2aae6b7565c2732ea6b4
wreevehicks <wrhicks@bu.edu>
2015-11-20 18:55:59 -0500

    Cases for infection and clear messages

---
+++ CasesFor_InfectionAndClear/CasesFor_InfectionAndClear.ino
@@ -0,0 +1,33 @@
+bool isInfected = false;
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+case MSG_INFECTION:
+  if (isLeader == false) {
+    isInfected = true;
+    digitalWrite(PIN_BLUE_LED, LOW);
+    digitalWrite(PIN_RED_LED, HIGH);
+    digitalWrite(PIN_GREEN_LED, LOW);
+    xbee.send(ZBTxRequest(XBeeAddress64(0x00000000, 0x0000FFFF, (uint8_t*) MSG_INFECT, 1));    
+  }
+  break;
+case MSG_CLEAR:
+  if (isInfected = true) {
+    isInfected = false;
+    digitalWrite(PIN_BLUE_LED, LOW);
+    digitalWrite(PIN_RED_LED, LOW);
+    digitalWrite(PIN_GREEN_LED, HIGH);
+    xbee.send(ZBTxRequest(XBeeAddress64(0x00000000, 0x0000FFFF, (uint8_t*) MSG_CLEAR, 1));    
+  }
+  break;
+
+}
+
+
