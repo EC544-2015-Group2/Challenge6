34c785adc93af2da39ab0f9311e57b692e4faa1b
wreevehicks <wrhicks@bu.edu>
2015-11-23 09:33:38 -0500

    Updated version

--- CasesFor_InfectionAndClear/CasesFor_InfectionAndClear.ino
+++ CasesFor_InfectionAndClear/CasesFor_InfectionAndClear.ino
@@ -1,9 +1,9 @@
 bool isInfected = false;
 int lastClearedTimestamp = 0;
-#define IMMUNITY_PERIOD = 3000;
+#define IMMUNITY_PERIOD 3000;
 
 int infectionTimeout = 0;
-#define INFECTION_BROADCAST_PERIOD = 3000;
+#define INFECTION_BROADCAST_PERIOD 3000;
 
 void setup() {
   // put your setup code here, to run once:
@@ -11,35 +11,41 @@ void setup() {
 }
 
 void loop() {
-  
+
+//--------- isInfected question
   if (isInfected) {
     if (millis() - infectionTimeout > INFECTION_BROADCAST_PERIOD) {
       txRequest = ZBTxRequest(XBeeAddress64(0x00000000, 0x0000FFFF, (uint8_t*) MSG_INFECT, 1));
       xbee.send(txRequest);
       infectionTimeout = millis();
-
-
-    case MSG_INFECTION:
-      if (millis() - lastClearedTimestamp  < IMMUNITY_PERIOD || isLeader) break;
-      isInfected = true;
-      digitalWrite(PIN_BLUE_LED, LOW);
-      digitalWrite(PIN_RED_LED, HIGH);
-      digitalWrite(PIN_GREEN_LED, LOW);
-      txRequest = ZBTxRequest(XBeeAddress64(0x00000000, 0x0000FFFF, (uint8_t*) MSG_INFECT, 1));
-      xbee.send(txRequest);
-      break;
-    case MSG_CLEAR:
-      if (isInfected = true) {
-        isInfected = false;
-        digitalWrite(PIN_BLUE_LED, LOW);
-        digitalWrite(PIN_RED_LED, LOW);
-        digitalWrite(PIN_GREEN_LED, HIGH);
-        ZBTxRequest(XBeeAddress64(0x00000000, 0x0000FFFF, (uint8_t*) MSG_CLEAR, 1));
-        xbee.send(txRequest);
-      }
-      lastClearedTimestamp = millis();
-      break;
-
     }
+  }
+
+//--------- OTHER MESSAGE CASES
+
+case MSG_INFECTION:
+  if (millis() - lastClearedTimestamp  < IMMUNITY_PERIOD || isLeader) break;
+  isInfected = true;
+  digitalWrite(PIN_BLUE_LED, LOW);
+  digitalWrite(PIN_RED_LED, HIGH);
+  digitalWrite(PIN_GREEN_LED, LOW);
+  txRequest = ZBTxRequest(XBeeAddress64(0x00000000, 0x0000FFFF, (uint8_t*) MSG_INFECT, 1));
+  xbee.send(txRequest);
+  break;
+case MSG_CLEAR:
+  if (isInfected = true) {
+    isInfected = false;
+    digitalWrite(PIN_BLUE_LED, LOW);
+    digitalWrite(PIN_RED_LED, LOW);
+    digitalWrite(PIN_GREEN_LED, HIGH);
+    ZBTxRequest(XBeeAddress64(0x00000000, 0x0000FFFF, (uint8_t*) MSG_CLEAR, 1));
+    xbee.send(txRequest);
+  }
+  lastClearedTimestamp = millis();
+  break;
+
+}
+
+
 
 
