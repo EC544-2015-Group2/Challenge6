0042877cb325a274465697d9720ce742207f733a
ghirlekar <gaurav.hirlekar@gmail.com>
2015-11-23 10:16:52 -0500

    Refactored entire code and this will probably work now. (Haven't tried it out though)

--- MoteCode/MoteCode.ino
+++ MoteCode/MoteCode.ino
@@ -5,31 +5,35 @@
 #define PIN_RED_LED               12    // infected
 #define PIN_GREEN_LED             13    // clear
 
-#define ELECTION_REPLY_TIMEOUT    1000
-#define ELECTION_VICTORY_TIMEOUT  2000
+#define ELECTION_REPLY_WAIT_PERIOD      1000
+#define ELECTION_VICTORY_WAIT_PERIOD    2000
+#define LEADER_HEARTBEAT_PERIOD         1000
 
 const uint8_t MSG_ELECTION = 0xB0,
               MSG_ACK = 0xB1,
               MSG_VICTORY = 0xB2,
               MSG_INFECTION = 0xB3,
               MSG_CLEAR = 0xB4,
-              MSG_DISCOVERY = 0xB5;
+              MSG_DISCOVERY = 0xB5,
+              MSG_HEARTBEAT = 0xB6;
 
-const uint8_t myCommand[] = {'M', 'Y'};
+const uint8_t slCommand[] = {'S', 'L'};
 
 XBee xbee = XBee();
 SoftwareSerial xbeeSerial(2, 3);
 ZBRxResponse rxResponse = ZBRxResponse();
 ZBTxRequest txRequest;
-AtCommandRequest atRequest = AtCommandRequest((uint8_t*)myCommand);
+AtCommandRequest atRequest = AtCommandRequest((uint8_t*)slCommand);
 AtCommandResponse atResponse;
 
-uint32_t myAddress64;   // 32-bit integer as the first 32 bits are same for all Xbee devices (0013a200)
+uint32_t myAddress64, leaderAddress64, remoteAddress64;   // 32-bit integer as the first 32 bits are same for all Xbee devices (0013a200)
 uint32_t listAddress64[10];
-uint32_t leaderAddress64;
 uint8_t numDevices = 0;
-bool isLeader = false;
 bool isInfected = false;
+
+bool isElecting = false, isAcknowledged = false;
+uint32_t electionTimeout, leaderHeartbeatTimeout;
+uint8_t heartbeatsLost = 0;
 
 void setup() {
   Serial.begin(57600);
@@ -38,39 +42,27 @@ void setup() {
   delay(2000);
 
   initLedPins();
-  myAddress64 = getMyAddress64();
-  broadcastMyAddress64();
+  getMyAddress64();
+  leaderAddress64 = myAddress64;
+  sendCommand(0x0000FFFF, (uint8_t*)&MSG_DISCOVERY, 1);
+  leaderHeartbeatTimeout = millis() + LEADER_HEARTBEAT_PERIOD;
 }
 
 void loop() {
-  if (xbee.readPacket(1) && xbee.getResponse().getApiId() == ZB_RX_RESPONSE) {
-    xbee.getResponse().getZBRxResponse(rxResponse);
-    if (rxResponse.getRemoteAddress64().getLsb() > myAddress64) {
-      isLeader = false;
-      leaderAddress64 = rxResponse.getRemoteAddress64().getLsb();
+  readAndHandlePackets();
+  if(isElecting && millis() > electionTimeout){
+    isElecting = false;
+    if (isAcknowledged) beginElection();
+    else {
+      sendCommand(0x0000FFFF, (uint8_t*)&MSG_VICTORY, 1);
+      leaderAddress64 = myAddress64;
     }
-    switch (rxResponse.getData(0)) {
-      case MSG_DISCOVERY:
-        uint32_t remoteAddress64;
-        for (int i = 1; i < 5; i++) remoteAddress64 |= rxResponse.getData(i) << (5 - i);
-        for (int i = 0; i < numDevices; i++)
-          if (listAddress64[i] == remoteAddress64)  break;
-          else listAddress64[numDevices++] = remoteAddress64;
-        break;
-
-      case MSG_ELECTION:
-        txRequest = ZBTxRequest(XBeeAddress64(0x0013a200, rxResponse.getRemoteAddress64().getLsb()), (uint8_t*) &MSG_ACK, 1);
-        xbee.send(txRequest);
-        beginElection();
-        break;
-
-      case MSG_VICTORY:
-        uint32_t newLeaderAddress64 = 0;
-        for (int i = 1; i < 5; i++)  newLeaderAddress64 |= rxResponse.getData(i) << (5 - i);
-        if (newLeaderAddress64 < myAddress64)  beginElection();
-        else leaderAddress64 = newLeaderAddress64;
-        break;
-    }
+  }
+  if(millis() > leaderHeartbeatTimeout){
+    if(leaderAddress64 == myAddress64){
+      sendCommand(0x0000FFFF, (uint8_t*) &MSG_HEARTBEAT, 1);
+      leaderHeartbeatTimeout = millis() + LEADER_HEARTBEAT_PERIOD / 2;
+    } else beginElection();
   }
 }
 
@@ -84,65 +76,78 @@ void initLedPins(void) {
   digitalWrite(PIN_GREEN_LED, HIGH);
 }
 
-uint32_t getMyAddress64(void) {
+void getMyAddress64(void) {
   do {
     do    xbee.send(atRequest);
     while (!xbee.readPacket(5000) || xbee.getResponse().getApiId() != AT_COMMAND_RESPONSE);
 
     xbee.getResponse().getAtCommandResponse(atResponse);
   } while (!atResponse.isOk());
-
-  return atResponse.getValue()[0] << 8 + atResponse.getValue()[1];
+  memcpy(&myAddress64, atResponse.getValue(), 4);
 }
 
-void broadcastMyAddress64(void) {
-  uint8_t discoverMessagePayload[5];
-  discoverMessagePayload[0] = MSG_DISCOVERY;
-  for (int i = 1; i < 5; i++)  discoverMessagePayload[i] = myAddress64 >> 8 * (i - 1) & 0xFF;
-  txRequest = ZBTxRequest(XBeeAddress64(0x00000000, 0x0000FFFF), discoverMessagePayload, 5);
+void sendCommand(uint32_t destinationAddress64, uint8_t* payload, uint8_t length) {
+  txRequest = ZBTxRequest(XBeeAddress64(0x00000000, destinationAddress64), payload, length);
   xbee.send(txRequest);
 }
 
 void beginElection(void) {
+  if (isElecting)  return;
+  else isElecting = true;
+  isAcknowledged = false;
   uint8_t countDevices = 0;
-  bool ack = false;
   for (int i = 0; i < numDevices; i++)
     if (listAddress64[i] > myAddress64) {
-      txRequest = ZBTxRequest(XBeeAddress64(0x0013a200, listAddress64[i]), (uint8_t*)MSG_ELECTION, 1);
-      xbee.send(txRequest);
+      sendCommand(listAddress64[i], (uint8_t*) &MSG_ELECTION, 1);
       countDevices++;
     }
-  if (countDevices > 0) {
-    uint32_t timeout = millis() + ELECTION_REPLY_TIMEOUT;
-    while (millis() < timeout) {
-      if (xbee.readPacket(1) && xbee.getResponse().getApiId() == ZB_RX_RESPONSE) {
-        xbee.getResponse().getZBRxResponse(rxResponse);
-        switch (rxResponse.getData(0)) {
-          case MSG_ELECTION:
-            txRequest = ZBTxRequest(XBeeAddress64(0x0013a200, rxResponse.getRemoteAddress64().getLsb()), (uint8_t*) MSG_ACK, 1);
-            xbee.send(txRequest);
-            break;
-          case MSG_ACK:
-            timeout = millis() + ELECTION_VICTORY_TIMEOUT;
-            ack = true;
-            break;
-          case MSG_VICTORY:
-            isLeader = false;
-            leaderAddress64 = 0;
-            for (int i = 1; i < 5; i++) leaderAddress64 |= rxResponse.getData(i) << (5 - i);
-            return;
+  if (countDevices > 0) electionTimeout = millis() + ELECTION_REPLY_WAIT_PERIOD;
+  else electionTimeout = millis();
+}
+
+void readAndHandlePackets(void) {
+  if (xbee.readPacket(1) && xbee.getResponse().getApiId() == ZB_RX_RESPONSE) {
+    xbee.getResponse().getZBRxResponse(rxResponse);
+    remoteAddress64 = rxResponse.getRemoteAddress64().getLsb();
+    if (remoteAddress64 > myLeaderAddress64) beginElection();     // VERIFY WHETHER YOU ACTUALLY NEED THIS
+    switch (rxResponse.getData(0)) {
+      case MSG_DISCOVERY:
+        if (rxResponse.getDataLength() > 1) {
+          memcpy(&leaderAddress64, rxResponse.getData() + 1, sizeof(leaderAddress64));
+        } else {
+          uint8_t msgPayload[5];
+          msgPayload[0] = MSG_DISCOVERY;
+          memcpy(msgPayload + 4, &leaderAddress64, sizeof(leaderAddress64));
+          sendCommand(remoteAddress64, msgPayload, 5);
         }
-      }
+
+        for (int i = 0; i < numDevices; i++)
+          if (listAddress64[i] == remoteAddress64)  break;
+        listAddress64[numDevices++] = remoteAddress64;
+        break;
+
+      case MSG_ELECTION:
+        sendCommand(remoteAddress64, (uint8_t*)&MSG_ACK, 1);
+        beginElection();
+        break;
+
+      case MSG_ACK:
+        if (isElecting) {
+          electionTimeout = millis() + ELECTION_VICTORY_WAIT_PERIOD;
+          isAcknowledged = true;
+        }
+        break;
+
+      case MSG_VICTORY:
+        if (remoteAddress64 > myAddress64) {
+          leaderAddress64 = remoteAddress64;
+          isElecting = false;
+        }
+        else beginElection();
+        break;
+
+      case MSG_HEARTBEAT:
+        leaderHeartbeatTimeout = millis() + LEADER_HEARTBEAT_PERIOD;
     }
-  }
-  if (ack) beginElection();
-  else {
-    uint8_t victoryMessagePayload[5];
-    victoryMessagePayload[0] = MSG_VICTORY;
-    for (int i = 1; i < 5; i++)  victoryMessagePayload[i] = myAddress64 >> 8 * (i - 1) & 0xFF;
-    txRequest = ZBTxRequest(XBeeAddress64(0x00000000, 0x0000FFFF), victoryMessagePayload, 5);
-    xbee.send(txRequest);
-    isLeader = true;
-    leaderAddress64 = myAddress64;
   }
 }
